7dbbecbf643bd382646f75ba9cff2c25fd4c16b2
Gilbert Torchon <gilbert@noukod.com>
2019-01-14 19:51:44 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -36,13 +36,13 @@ class PetsController < ApplicationController
   end
 
   patch '/pets/:id' do
+    raise params.inspect
     @pet=Pet.find_by_id(@params[:id])
     @pet.name=@params[:pet_name]
     @pet.owner=Owner.find_by_id(@params[:pet][:owner_id])
     #binding.pry
 
     @pet.owner = Owner.create @params[:owner] unless params[:owner][:name].empty?
-
 
     @pet.save
 
